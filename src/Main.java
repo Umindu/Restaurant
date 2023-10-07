36da3c509abeee45758cd62784cdf4f993d3358a
Umindu <ybumindu@gmail.com>
2023-10-07 10:35:49 +0530

    1

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+//import javafx.stage.StageStyle;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        // Create an instance of the FXMLLoader class.
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Dashboard/Dashboard.fxml"));
+
+        // Load the FXML file.
+        Parent root = (Parent) loader.load();
+
+        //stage.initStyle(StageStyle.UNDECORATED);
+
+        // Set the scene on the stage.
+        stage.setScene(new Scene(root));
+
+        // Show the stage.
+        stage.show();
+    }
+}
