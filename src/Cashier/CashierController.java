3be5fcb8b640cc1c27bb022ed9a1503e1b28aae7
Umindu <ybumindu@gmail.com>
2023-10-23 12:46:47 +0530

    22

--- src/Cashier/CashierController.java
+++ src/Cashier/CashierController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
 public class CashierController {
@@ -12,21 +13,21 @@ public class CashierController {
     private HBox summaryBar;
 
     @FXML
-    private VBox tabOne;
+    private Pane tabOne;
 
     @FXML
     private VBox tabTwo;
 
     @FXML
-    void showTabOne(ActionEvent event) {
-        tabOne.setVisible(false);
-        tabTwo.setVisible(true);
+    void showDrawer(ActionEvent event) {
+        tabOne.setVisible(true);
+        tabTwo.setVisible(false);
     }
 
     @FXML
     void showTodaySale(ActionEvent event) {
-        tabOne.setVisible(true);
-        tabTwo.setVisible(false);
+        tabOne.setVisible(false);
+        tabTwo.setVisible(true);
         summaryBar.setPrefHeight(130);
         summaryBar.setVisible(true);
         VBox.setMargin(summaryBar, new Insets(20, 0, 0, 0));
@@ -34,8 +35,8 @@ public class CashierController {
 
     @FXML
     void showSaleHistory(ActionEvent event) {
-        tabOne.setVisible(true);
-        tabTwo.setVisible(false);
+        tabOne.setVisible(false);
+        tabTwo.setVisible(true);
         summaryBar.setPrefHeight(0);
         summaryBar.setVisible(false);
         VBox.setMargin(summaryBar, new Insets(0, 0, 0, 0));
