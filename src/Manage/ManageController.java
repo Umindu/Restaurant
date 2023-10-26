6b60c48e39d2483f9abdbd8fc9637b53afe1ae82
Umindu <ybumindu@gmail.com>
2023-10-26 22:32:30 +0530

    27

--- src/Manage/ManageController.java
+++ src/Manage/ManageController.java
@@ -1,17 +1,39 @@
 package Manage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import Manage.Products.ProductController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Control;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 
-public class ManageController {
+public class ManageController implements Initializable {
+
+    @FXML
+    private Button proMainBtn;
+
+    @FXML
+    private Button addProBtn;
+
+    @FXML
+    private Button allProBtn;
+
+    @FXML
+    private Button categoryBtn;
+
+    @FXML
+    private Button emploMainBtn;
+
+    @FXML
+    private Button tableMainBtn;
 
     @FXML
     private BorderPane borderPane;
@@ -25,6 +47,7 @@ public class ManageController {
     @FXML
     private VBox tableVbox;
 
+    //product window
     @FXML
     private static VBox addProPane;
 
@@ -40,7 +63,27 @@ public class ManageController {
         categpryPane = categpryPane2;
     }
 
-    // .....................................
+    // Reset button style.....................
+    public void ResetButtonStyle(){
+        proMainBtn.setStyle("-fx-background-color : #fff; -fx-text-fill: #000; -fx-background-radius: 10 10 0 0;");
+        addProBtn.setStyle("-fx-background-color : #fff; -fx-text-fill: #000;");
+        allProBtn.setStyle("-fx-background-color : #fff; -fx-text-fill: #000;");
+        categoryBtn.setStyle("-fx-background-color : #fff; -fx-text-fill: #000;");
+        emploMainBtn.setStyle("-fx-background-color : #fff; -fx-text-fill: #000;");
+        tableMainBtn.setStyle("-fx-background-color : #fff; -fx-text-fill: #000;");
+    }
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        try {
+            AnchorPane view = FXMLLoader.load(getClass().getResource("Products/Products.fxml"));
+            borderPane.setCenter(view);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Product tab.....................................
     @FXML
     void showProductVbox(ActionEvent event) throws IOException {
         AnchorPane view = FXMLLoader.load(getClass().getResource("Products/Products.fxml"));
@@ -52,27 +95,35 @@ public class ManageController {
             productVbox.setVisible(true);
             productVbox.setPrefHeight(Control.USE_COMPUTED_SIZE);
         }
+        ResetButtonStyle();
+        proMainBtn.setStyle("-fx-background-color : #fe8b2c !important; -fx-background-radius: 10 10 0 0; -fx-text-fill: #fff;");
     }
 
     @FXML
     void showAddProduct(ActionEvent event) {
         ProductController pController = new ProductController();
         pController.showAddProPane(allProPane, addProPane, categpryPane);
+        ResetButtonStyle();
+        addProBtn.setStyle("-fx-background-color : #fe8b2c !important; -fx-text-fill: #fff;");
     }
 
     @FXML
     void showAllProducts(ActionEvent event) {
         ProductController pController = new ProductController();
         pController.showAllProPane(allProPane, addProPane, categpryPane);
+        ResetButtonStyle();
+        allProBtn.setStyle("-fx-background-color : #fe8b2c !important; -fx-text-fill: #fff;");
     }
 
     @FXML
     void showCategory(ActionEvent event) {
         ProductController pController = new ProductController();
         pController.showCategoryPane(allProPane, addProPane, categpryPane);
+        ResetButtonStyle();
+        categoryBtn.setStyle("-fx-background-color : #fe8b2c !important; -fx-text-fill: #fff;");
     }
 
-    // ......................................
+    // Employee tab......................................
     @FXML
     void showEmployeeVbox(ActionEvent event) {
         if (employeeVbox.isVisible()) {
@@ -84,6 +135,8 @@ public class ManageController {
         }
     }
 
+
+    // Table tab......................................
     @FXML
     void showTableVbox(ActionEvent event) {
         if (tableVbox.isVisible()) {
@@ -94,4 +147,5 @@ public class ManageController {
             tableVbox.setPrefHeight(Control.USE_COMPUTED_SIZE);
         }
     }
+
 }
