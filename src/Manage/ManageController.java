11111e5a3232887e54e4ece518ff118e5e025825
Umindu <ybumindu@gmail.com>
2023-10-26 16:39:10 +0530

    26

--- src/Manage/ManageController.java
+++ src/Manage/ManageController.java
@@ -1,11 +1,20 @@
 package Manage;
 
+import java.io.IOException;
+
+import Manage.Products.ProductController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Control;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 
 public class ManageController {
+
+    @FXML
+    private BorderPane borderPane;
 
     @FXML
     private VBox productVbox;
@@ -17,7 +26,25 @@ public class ManageController {
     private VBox tableVbox;
 
     @FXML
-    void showProductVbox(ActionEvent event) {
+    private static VBox addProPane;
+
+    @FXML
+    private static VBox allProPane;
+
+    @FXML
+    private static VBox categpryPane;
+
+    public static void setPane(VBox allProPane2, VBox addProPane2, VBox categpryPane2) {
+        addProPane = addProPane2;
+        allProPane = allProPane2;
+        categpryPane = categpryPane2;
+    }
+
+    // .....................................
+    @FXML
+    void showProductVbox(ActionEvent event) throws IOException {
+        AnchorPane view = FXMLLoader.load(getClass().getResource("Products/Products.fxml"));
+        borderPane.setCenter(view);
         if (productVbox.isVisible()) {
             productVbox.setVisible(false);
             productVbox.setPrefHeight(0);
@@ -27,6 +54,25 @@ public class ManageController {
         }
     }
 
+    @FXML
+    void showAddProduct(ActionEvent event) {
+        ProductController pController = new ProductController();
+        pController.showAddProPane(allProPane, addProPane, categpryPane);
+    }
+
+    @FXML
+    void showAllProducts(ActionEvent event) {
+        ProductController pController = new ProductController();
+        pController.showAllProPane(allProPane, addProPane, categpryPane);
+    }
+
+    @FXML
+    void showCategory(ActionEvent event) {
+        ProductController pController = new ProductController();
+        pController.showCategoryPane(allProPane, addProPane, categpryPane);
+    }
+
+    // ......................................
     @FXML
     void showEmployeeVbox(ActionEvent event) {
         if (employeeVbox.isVisible()) {
--- src/Manage/Products/ProductController.java
+++ src/Manage/Products/ProductController.java
@@ -1,5 +1,47 @@
 package Manage.Products;
 
-public class ProductController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import Manage.ManageController;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.VBox;
+
+public class ProductController implements Initializable{
+
+    @FXML
+    private VBox addProPane;
+
+    @FXML
+    private VBox allProPane;
+
+    @FXML
+    private VBox categoryPane;
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        ManageController.setPane(allProPane ,addProPane, categoryPane);
+    }
+
+    public void showAllProPane(VBox allProPane2, VBox addProPane2, VBox categoryPane2){
+        addProPane2.setVisible(false);
+        categoryPane2.setVisible(false);
+        allProPane2.setVisible(true);
+    }
+
+    public void showAddProPane(VBox allProPane2, VBox addProPane2, VBox categoryPane2){
+        allProPane2.setVisible(false);
+        categoryPane2.setVisible(false);
+        addProPane2.setVisible(true);
+    }
+
+    public void showCategoryPane(VBox allProPane2, VBox addProPane2, VBox categoryPane2){
+        allProPane2.setVisible(false);
+        addProPane2.setVisible(false);
+        categoryPane2.setVisible(true);
+    }
+
+    
     
 }
