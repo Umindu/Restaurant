625301f4febdfde792b42f4eea92f1e83d9dfa8e
Umindu <ybumindu@gmail.com>
2023-11-02 13:19:41 +0530

    31

--- src/Manage/Products/ProductController.java
+++ src/Manage/Products/ProductController.java
@@ -8,6 +8,7 @@ import java.util.ResourceBundle;
 import DataBase.DBConnect;
 import Manage.ManageController;
 import Manage.Products.model.Category;
+import Manage.Products.model.Product;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -19,16 +20,79 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
 
 public class ProductController implements Initializable{
 
     @FXML
+    private Label addAndEditLableTitle;
+
+    //Add Products .........................................
+
+    @FXML
+    private Label addProError;
+
+    @FXML
+    private TextField proIdTextField;
+
+    @FXML
+    private TextField proNameTextField;
+
+    @FXML
+    private ComboBox<String> proCategoryCombo;
+
+    @FXML
+    private TextField proCostTextField;
+
+    @FXML
+    private TextField proPriceTextField;
+
+    @FXML
+    private TextField proDiscountTextField;
+
+    @FXML
+    private TextArea proDescriptionTextArea;
+
+    @FXML
     private VBox addProPane;
+
+    private ObservableList<String> categoryComboList = FXCollections.observableArrayList();
+
+
+    //All Products .........................................
+    @FXML
+    private TableView<Product> productsTableView;
+
+    @FXML
+    private TableColumn<Product, String> tableProID;
+
+    @FXML
+    private TableColumn<Product, String> tableProName;
+
+    @FXML
+    private TableColumn<Product, String> tableProCategory;
+
+    @FXML
+    private TableColumn<Product, String> tableProCost;
+
+    @FXML
+    private TableColumn<Product, String> tableProDiscount;
+
+    @FXML
+    private TableColumn<Product, String> tableProPrice;
+
+    @FXML
+    private TableColumn<Product, HBox> tableProAction;
+
+    private ObservableList<Product> productList = FXCollections.observableArrayList();
 
     @FXML
     private VBox allProPane;
+
 
     //Category .........................................
     @FXML
@@ -54,19 +118,184 @@ public class ProductController implements Initializable{
     @FXML
     private TextField catNameTextField;
 
+
+
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
         ManageController.setProductsPane(allProPane ,addProPane, categoryPane);
-
+        
+        //category table 
         tableCatID.setCellValueFactory(new PropertyValueFactory<Category ,String>("CategoryId"));
         tableCatName.setCellValueFactory(new PropertyValueFactory<Category ,String>("CategoryName"));
         tableCatAction.setCellValueFactory(new PropertyValueFactory<Category ,HBox>("CategoryAction"));
 
         categoryTableView.setItems(categoryList);
-        loadDataFromDatabase();
+        CategoryloadDataFromDatabase();
+
+        //product table
+        tableProID.setCellValueFactory(new PropertyValueFactory<Product ,String>("ProductId"));
+        tableProName.setCellValueFactory(new PropertyValueFactory<Product ,String>("ProductName"));
+        tableProCategory.setCellValueFactory(new PropertyValueFactory<Product ,String>("ProductCategory"));
+        tableProCost.setCellValueFactory(new PropertyValueFactory<Product ,String>("ProductCost"));
+        tableProDiscount.setCellValueFactory(new PropertyValueFactory<Product ,String>("ProductDiscount"));
+        tableProPrice.setCellValueFactory(new PropertyValueFactory<Product ,String>("ProductPrice"));
+        tableProAction.setCellValueFactory(new PropertyValueFactory<Product ,HBox>("ProductAction"));
+
+        productsTableView.setItems(productList);
+        ProductloadDataFromDatabase();
+
+        //product category combo
+        categoryComboList.add("Select Category");
+        try {
+            Statement statement = DBConnect.connectToDB().createStatement();
+            statement.execute("select * from Category");
+            ResultSet resultSet = statement.getResultSet();
+            while (resultSet.next()) {
+                String name = resultSet.getString("Name");
+                categoryComboList.add(name);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        proCategoryCombo.setItems(categoryComboList);
+        proCategoryCombo.getSelectionModel().select(0);
     }
 
-    public void loadDataFromDatabase() {
+    //Add Product #########################################################################
+    @FXML
+    void cancelProduct(ActionEvent event) {
+        proIdTextField.clear();
+        proNameTextField.clear();
+        proCategoryCombo.getSelectionModel().select(0);
+        proCostTextField.clear();
+        proPriceTextField.clear();
+        proDiscountTextField.clear();
+        proDescriptionTextArea.clear();
+
+        if (!proIdTextField.isEditable()) {
+            addProPane.setVisible(false);
+            allProPane.setVisible(true);
+        }
+        proIdTextField.setEditable(true);
+
+        addAndEditLableTitle.setText("Add Product");
+    }
+
+    @FXML
+    void saveProduct(ActionEvent event) {
+        String proId = proIdTextField.getText();
+        String proName = proNameTextField.getText();
+        String proCategory = proCategoryCombo.getSelectionModel().getSelectedItem();
+        String proCost = proCostTextField.getText();
+        String proPrice = proPriceTextField.getText();
+        String proDiscount = proDiscountTextField.getText();
+        String proDescription = proDescriptionTextArea.getText();
+
+        if (!proId.isEmpty() && !proName.isEmpty() && proCategory != "Select Category" && !proCost.isEmpty() && !proPrice.isEmpty()) {
+            //save to database
+            if (proIdTextField.isEditable()) {
+                try {
+                    Statement statement = DBConnect.connectToDB().createStatement();
+                    statement.execute("insert into Products values ('"+proId+"', '"+proName+"', '"+proCategory+"', '"+proCost+"', '"+proDiscount+"', '"+proPrice+"', '"+proDescription+"')");
+                    System.out.println("Product saved successfully");
+                
+                    cancelProduct(null);
+                    ProductloadDataFromDatabase();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }else{
+                try {
+                    Statement statement = DBConnect.connectToDB().createStatement();
+                    statement.execute("update Products set Name = '"+proName+"', Category = '"+proCategory+"', Cost = '"+proCost+"', Discount = '"+proDiscount+"', Price = '"+proPrice+"', Description = '"+proDescription+"' where ID = '"+proId+"'");
+                    System.out.println("Product updated successfully");
+                    
+                    cancelProduct(null);
+                    ProductloadDataFromDatabase();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            
+        }else{
+            addProError.setVisible(true);
+        }
+    }
+
+    //All Product #########################################################################
+    public void ProductloadDataFromDatabase() {
+        productList.clear();
+        try {
+            Statement statement = DBConnect.connectToDB().createStatement();
+            statement.execute("select * from Products");
+            ResultSet resultSet = statement.getResultSet();
+            while (resultSet.next()) {
+                String id = resultSet.getString("ID");
+                String name = resultSet.getString("Name");
+                String category = resultSet.getString("Category");
+                String cost = resultSet.getString("Cost");
+                String discount = resultSet.getString("Discount");
+                String price = resultSet.getString("Price");
+                String description = resultSet.getString("Description");
+
+                Button editBtn = new Button("Edit");
+                editBtn.setId("actionEditBtn");
+                Button deleteBtn = new Button("Delete");
+                deleteBtn.setId("actionDeleteBtn");
+                HBox actionBtnContainer = new HBox(editBtn, deleteBtn);
+                actionBtnContainer.setId("actionBtnContainer");
+                editBtn.setOnAction(e -> {
+                    editProduct(new Product(id, name, category, cost, discount, price, description, actionBtnContainer));
+                });
+                deleteBtn.setOnAction(e ->{
+                    deleteProduct(new Product(id, name, category, cost, discount, price, description, actionBtnContainer));
+                });
+
+                productList.add(new Product(id, name, category, cost, discount, price, description, actionBtnContainer));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Add or Edit Products pane function.........................................
+    private void editProduct(Product product){
+        proIdTextField.setText(product.getProductId());
+        proNameTextField.setText(product.getProductName());
+        proCategoryCombo.getSelectionModel().select(product.getProductCategory());
+        proCostTextField.setText(product.getProductCost());
+        proPriceTextField.setText(product.getProductPrice());
+        proDiscountTextField.setText(product.getProductDiscount());
+        proDescriptionTextArea.setText(product.getProductDescription());
+        proIdTextField.setEditable(false);
+
+        allProPane.setVisible(false);
+        addProPane.setVisible(true);
+
+        addAndEditLableTitle.setText("Edit Product");
+    }
+
+    private void deleteProduct(Product product){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Delete Product");
+        alert.setHeaderText("Are you sure you want to delete this product?");
+        alert.showAndWait().ifPresent(response -> {
+            if (response == javafx.scene.control.ButtonType.OK) {
+                try {
+                    Statement statement = DBConnect.connectToDB().createStatement();
+                    statement.execute("delete from Products where ID = '"+product.getProductId()+"'");
+                    System.out.println("Product deleted successfully");
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                ProductloadDataFromDatabase();
+            }
+        });
+    }
+
+
+    //Category ############################################################################
+    public void CategoryloadDataFromDatabase() {
         categoryList.clear();
         try {
             Statement statement = DBConnect.connectToDB().createStatement();
@@ -77,8 +306,11 @@ public class ProductController implements Initializable{
                 String name = resultSet.getString("Name");
 
                 Button editBtn = new Button("Edit");
+                editBtn.setId("actionEditBtn");
                 Button deleteBtn = new Button("Delete");
+                deleteBtn.setId("actionDeleteBtn");
                 HBox actionBtnContainer = new HBox(editBtn, deleteBtn);
+                actionBtnContainer.setId("actionBtnContainer");
                 editBtn.setOnAction(e -> {
                     editCategory(new Category(id, name, actionBtnContainer));
                 });
@@ -113,7 +345,7 @@ public class ProductController implements Initializable{
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
-                loadDataFromDatabase();
+                CategoryloadDataFromDatabase();
             }
         });
     }
@@ -130,7 +362,7 @@ public class ProductController implements Initializable{
         String catId = catIdTextField.getText();
         String catName = catNameTextField.getText();
 
-        if(!catId.isEmpty() || !catName.isEmpty()){
+        if(!catId.isEmpty() && !catName.isEmpty()){
             //save to database
             if (catIdTextField.isEditable()) {
                 try {
@@ -151,7 +383,7 @@ public class ProductController implements Initializable{
             }
         }
         cancelCategory(null);
-        loadDataFromDatabase();
+        CategoryloadDataFromDatabase();
     }
 
     //Menu Button functions.........................................
---
+++ src/Manage/Products/model/Product.java
@@ -0,0 +1,50 @@
+package Manage.Products.model;
+
+import javafx.scene.layout.HBox;
+
+public class Product {
+    private String proId;
+    private String proName;
+    private String proCategory;
+    private String proCost;
+    private String proDiscount;
+    private String proPrice;
+    private String proDescription;
+    private HBox actionBtnContainer;
+
+    public Product(String proId, String proName, String proCategory, String proCost, String proDiscount, String proPrice, String proDescription, HBox actionBtnContainer) {
+        this.proId = proId;
+        this.proName = proName;
+        this.proCategory = proCategory;
+        this.proCost = proCost;
+        this.proDiscount = proDiscount;
+        this.proPrice = proPrice;
+        this.proDescription = proDescription;
+        this.actionBtnContainer = actionBtnContainer;
+    }
+
+    public String getProductId() {
+        return proId;
+    }
+    public String getProductName() {
+        return proName;
+    }
+    public String getProductCategory() {
+        return proCategory;
+    }
+    public String getProductCost() {
+        return proCost;
+    }
+    public String getProductDiscount() {
+        return proDiscount;
+    }
+    public String getProductPrice() {
+        return proPrice;
+    }
+    public String getProductDescription() {
+        return proDescription;
+    }
+    public HBox getProductAction() {
+        return actionBtnContainer;
+    }
+}
